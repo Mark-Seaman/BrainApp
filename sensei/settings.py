9ff7c37ca90cb7ab1e74e487a8366c2432af00d2
Sensei Admin <sensei@sensei.do>
2019-05-23 18:20:07 +0000

    Ignore git files

--- sensei/settings.py
+++ sensei/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$itxljw#m_u#=)av=e4s-yrl^yc%z39m$whh%hej54z_n8^@q4'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
