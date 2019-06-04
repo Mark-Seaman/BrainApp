a394120e76c3e252ab91fa722ce36894579f775d
Mark Seaman <Mark.Seaman@imac.net>
2019-06-04 13:43:24 -0600

    Use template views

--- brain/urls.py
+++ brain/urls.py
@@ -2,40 +2,13 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.http import HttpResponse
 
-from .views import BrainView, HomeView
+from .views import BrainView, HomeView, IndexView
 
-
-# Home View
-def home(request):
-    title = "Brain Page"
-    text = '''
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/brain/">Brain</a></li>
-        <li><a href="index">Index</a></li>
-    </ul>
-
-    '''
-    return HttpResponse("<h1>%s</h1><p>%s</p>" % (title,text))
-
-# Index View
-def index(request):
-    title = "Brain Index Page"
-    text = '''
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/brain/">Brain</a></li>
-        <li><a href="index">Index</a></li>
-    </ul>
-    '''
-    return HttpResponse("<h1>%s</h1><p>%s</p>" % (title,text))
- 
 
 # URL Routes 
 urlpatterns = [
-    # url(r'^$', home),
+#    url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view()),
-    url(r'^admin/', admin.site.urls),
-    url(r'^index$', index),
+    url(r'^index$', IndexView.as_view()),
     url(r'^brain$', BrainView.as_view()),
 ]
