5731a62de8b01aacb8311dc0d49c7d3e5fe67dc8
Sensei Admin <sensei@sensei.do>
2019-06-04 15:24:31 +0000

    SSL configuration

--- bashrc
+++ bashrc
@@ -3,4 +3,9 @@
 
 figlet 'Sensei Production Server'
 
+alias h='history 60'
+alias gs='git status'
+alias co='~/commit'
+
+ls -l
 
--- sensei/settings.py
+++ sensei/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['157.230.163.78', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['157.230.163.78', '127.0.0.1', 'localhost', 'seamanfamily.org']
 
 
 # Application definition
