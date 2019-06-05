caa1fd9bb4ec5a50457648230848decc2a195d54
Mark Seaman <Mark.Seaman@imac.net>
2019-06-05 10:41:10 -0600

    Add markdown document formatter

--- brain/views.py
+++ brain/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from subprocess import Popen, PIPE
-from sys import version_info
-
-# Create your views here.
 from django.views.generic import RedirectView, TemplateView
 from django.utils.timezone import now
+from subprocess import Popen, PIPE
+from sys import version_info
 
 
 class HomeView(TemplateView):
@@ -22,23 +20,13 @@ class BrainView(TemplateView):
         header = dict(title='Brain App Demo', subtitle='Brain Page Template')
         return dict(title=title, text="no text", header=header, time=now())
 
-markdown = '''# Markdown Text Format
-## Cheatsheet for formats
 
-Paragraph
-
-* Bullet list
-* List item
-
-1. Numbered list
-1. List item
-
-'''
 class MarkdownView(TemplateView):
     template_name = 'markdown.html'
 
     def get_context_data(self, **kwargs):
         title = 'Markdown View'
+        markdown = open('Documents/seamanfamily/brain/Markdown.md').read()
         text = markdown_to_html(markdown)
         return dict(title=title, text=text)
     
