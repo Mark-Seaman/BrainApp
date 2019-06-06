23c82fe45009a9cf23ed9b745c1dbb301b463945
Mark Seaman <Mark.Seaman@imac.net>
2019-06-06 14:11:56 -0600

    Test for Brain

--- brain/views.py
+++ brain/views.py
@@ -1,12 +1,17 @@
+from django.http import HttpResponseRedirect
 from django.views.generic import RedirectView, TemplateView
 from django.utils.timezone import now
+from os.path import join
 
-from brain.brain import list_files
-from .brain import document_html
+from .brain import document_html, list_files
 
 
 class RedirectPage(RedirectView):
     url = '/seamanfamily/brain/'
+
+    def get_redirect_url(self, *args, **kwargs):
+        path = join('/', self.request.path[1:], 'Index')
+
 
 
 # Display the document that matches the URL
@@ -16,7 +21,6 @@ class FolderView(TemplateView):
     def get_context_data(self, **kwargs):
         title = 'Folder View'
         path = self.kwargs.get('title', 'Index')
-        # path = 'seamanfamily/brain'
         files = list_files(path)
         header = dict(title='Brain App Demo', subtitle='Folder View')
         return dict(header=header, title=title, files=files, time=now())
@@ -33,5 +37,10 @@ class DocView(TemplateView):
         header = dict(title='Brain App Demo', subtitle='Document View')
         return dict(header=header, title=title, text=text, time=now())
 
-
-
+    # def get(self, request, *args, **kwargs):
+    #     url = doc_page(self.request.path[1:])
+    #     if url:
+    #         log('REDIRECT: %s --> %s' % (title, url))
+    #         return HttpResponseRedirect('/%s/' % url)
+    #
+    #     return self.render_to_response(self.get_context_data(**kwargs))
