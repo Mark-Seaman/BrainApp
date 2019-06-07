d41588ba0d57bdd04592f51836309cc198125c1b
Mark Seaman <Mark.Seaman@imac.net>
2019-06-07 10:58:29 -0600

    Simplify page settings

--- brain/urls.py
+++ brain/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     url(r'^$',                              RedirectRoot.as_view()),
     url(r'^(?P<title>[\w/\-.]*)/Index$',    IndexView.as_view()),
     url(r'^(?P<title>[\w/\-.]*)/Missing$',  MissingView.as_view()),
-    # url(r'^(?P<title>[\w/\-.]*)/$',         RedirectIndex.as_view()),
     url(r'^(?P<title>[\w/\-.]*)$',          DocView.as_view()),
 ]
--- brain/views.py
+++ brain/views.py
@@ -5,18 +5,33 @@ from os.path import exists, join, isdir
 
 from .brain import  doc_list, doc_redirect, render_doc
 
-
 # # Display the document that matches the URL
-# class FolderView(TemplateView):
-#     template_name = 'folder.html'
+# class BrainView(TemplateView):
+#     template_name = 'doc.html'
 #
 #     def get_context_data(self, **kwargs):
 #         doc = self.request.path[1:]
 #         path = join('Documents', doc)
-#         if exists(path) and isdir(path):
-#             docs = doc_list(doc)
-#             header = dict(title='Brain', subtitle='Folder View')
-#             return dict(header=header, title=doc, docs=docs, time=now())
+#         text = render_doc(doc)
+#         header = dict(title='Brain', subtitle='Document View')
+#         return dict(header=header, title=path, text=text, time=now())
+#
+#     def get(self, request, *args, **kwargs):
+#         doc = self.request.path[1:]
+#         while doc.endswith('/'):
+#             doc = doc[:-1]
+#         url = doc_redirect(doc)
+#         if url:
+#             return HttpResponseRedirect(url)
+#         return self.render_to_response(self.get_context_data(**kwargs))
+
+
+def page_settings(**kwargs):
+    path = kwargs.get('path')
+    header = dict(title='My Notes', subtitle=path)
+    settings = dict(header=header, title=path, time=now())
+    settings.update(kwargs)
+    return settings
 
 
 # Display the document that matches the URL
@@ -27,8 +42,9 @@ class DocView(TemplateView):
         doc = self.request.path[1:]
         path = join('Documents', doc)
         text = render_doc(doc)
-        header = dict(title='Brain', subtitle='Document View')
-        return dict(header=header, title=path, text=text, time=now())
+        # header = dict(title='My Notes', subtitle=path)
+        # return dict(header=header, title=path, text=text, time=now())
+        return page_settings(path=path, text=text)
 
     def get(self, request, *args, **kwargs):
         doc = self.request.path[1:]
@@ -41,7 +57,7 @@ class DocView(TemplateView):
 
 
 # Display the document that matches the URL
-class IndexView(TemplateView):
+class IndexView(DocView):
     template_name = 'folder.html'
 
     def get_context_data(self, **kwargs):
@@ -51,8 +67,10 @@ class IndexView(TemplateView):
         if exists(path) and isdir(path):
             docs = doc_list(title)
             text = render_doc(doc)
-            header = dict(title='Brain', subtitle='Index View')
-            return dict(header=header, title=doc, docs=docs, text=text, time=now())
+            # header = dict(title='My Notes', subtitle=path)
+            # return dict(header=header, title=doc, docs=docs, text=text, time=now())
+            return page_settings(path=path, docs=docs, text=text)
+
 
 
 # Display the document that matches the URL
@@ -61,15 +79,11 @@ class MissingView(TemplateView):
 
     def get_context_data(self, **kwargs):
         path = join('Documents', self.kwargs.get('title'))
-        title = 'Missing Document'
-        header = dict(title='Brain', subtitle='Index View')
-        return dict(header=header, title=title, path=path, time=now())
-
-
-# # Forward from directory to Index
-# class RedirectIndex(RedirectView):
-#     def get_redirect_url(self, *args, **kwargs):
-#         return join(self.request.path[:-1], 'Index')
+        text = 'Missing Document'
+        # title = 'Missing Document'
+        # header = dict(title='My Notes', subtitle=path)
+        # return dict(header=header, title=title, path=path, time=now())
+        return page_settings(path=path, text=text)
 
 
 # Forward from / to /seamanfamily/brain/Index
