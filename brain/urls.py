d7efac9d907184ea50e6cf0f71bc023ef66fa5ef
Mark Seaman <Mark.Seaman@imac.net>
2019-06-07 09:50:46 -0600

    Auto CSS refresh

--- brain/urls.py
+++ brain/urls.py
@@ -1,27 +1,12 @@
 from django.conf.urls import url
 
-from .views import DocView, FolderView, IndexView, RedirectIndex, RedirectRoot
+from .views import DocView, IndexView, RedirectIndex, RedirectRoot
 
 
 # URL Routes
 urlpatterns = [
-
     url(r'^$',                              RedirectRoot.as_view()),
     url(r'^(?P<title>[\w/\-.]*)/Index$',    IndexView.as_view()),
     url(r'^(?P<title>[\w/\-.]*)/$',         RedirectIndex.as_view()),
     url(r'^(?P<title>[\w/\-.]*)$',          DocView.as_view()),
 ]
-
-# url(r'^(?P<title>[\w/.]*)/$',           FolderView.as_view()),
-
-# from django.conf import settings
-    # from django.conf.urls.static import static
-    # from django.contrib import admin
-
-    #    url(r'^admin/', admin.site.urls),
-    # url(r'^brain/Index$',               BrainView.as_view()),
-    # url(r'^markdown$',                  MarkdownView.as_view()),
-    # url(r'^brain/redirect$',            RedirectPage.as_view()),
-
-
-# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- brain/views.py
+++ brain/views.py
@@ -6,15 +6,6 @@ from os.path import exists, join, isdir
 from .brain import document_html, doc_list, doc_redirect, list_files, render_doc
 
 
-class RedirectRoot(RedirectView):
-    url = '/seamanfamily/brain/'
-
-
-class RedirectIndex(RedirectView):
-    def get_redirect_url(self, *args, **kwargs):
-        return join(self.request.path[:-1], 'Index')
-
-
 # Display the document that matches the URL
 class FolderView(TemplateView):
     template_name = 'folder.html'
@@ -23,7 +14,6 @@ class FolderView(TemplateView):
         doc = self.request.path[1:]
         path = join('Documents', doc)
         if exists(path) and isdir(path):
-            # files = list_files(doc)
             docs = doc_list(doc)
             header = dict(title='Brain', subtitle='Folder View')
             return dict(header=header, title=doc, docs=docs, time=now())
@@ -52,10 +42,6 @@ class DocView(TemplateView):
 
     def get_context_data(self, **kwargs):
         doc = self.request.path[1:]
-        # path = join('Documents', doc)
-        # if not exists(path) and exists(path+'.md'):
-        #     doc = doc+'.md'
-        # text = document_html(doc)
         path = join('Documents', doc)
         text = render_doc(doc)
         header = dict(title='Brain', subtitle='Document View')
@@ -67,5 +53,17 @@ class DocView(TemplateView):
         if url:
             return HttpResponseRedirect(url)
         return self.render_to_response(self.get_context_data(**kwargs))
+
+
+# Forward from directory to Index
+class RedirectIndex(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return join(self.request.path[:-1], 'Index')
+
+
+# Forward from / to /seamanfamily/brain/Index
+class RedirectRoot(RedirectView):
+    url = '/seamanfamily/brain/'
+
 
 
