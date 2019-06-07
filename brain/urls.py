6fc55da46302a878e0cc9ef3dd58570806e243d8
Mark Seaman <Mark.Seaman@imac.net>
2019-06-07 13:09:53 -0600

    Info days

--- brain/urls.py
+++ brain/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
-from .views import DocView, IndexView, MissingView, RedirectRoot
+from .views import DocView, FilesView, IndexView, MissingView, RedirectRoot
 
 
 # URL Routes
 urlpatterns = [
     url(r'^$',                              RedirectRoot.as_view()),
+    url(r'^(?P<title>[\w/\-.]*)/Files$',    FilesView.as_view()),
     url(r'^(?P<title>[\w/\-.]*)/Index$',    IndexView.as_view()),
     url(r'^(?P<title>[\w/\-.]*)/Missing$',  MissingView.as_view()),
     url(r'^(?P<title>[\w/\-.]*)$',          DocView.as_view()),
